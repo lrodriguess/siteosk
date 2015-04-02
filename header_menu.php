86a3f5646609c908a4d7da3f163872dbe401f21e
léo rodrigues <leonardopontorodrigues@gmail.com>
2015-04-02 18:15:25 -0300

    menu coleções anteriores
    falta tradução em japonês

--- header_menu.php
+++ header_menu.php
@@ -26,7 +26,10 @@
           
           <?php if ($lang == "en") { ?>
  	           <li><a href="/inhotim_en.php" title="Inhotim">Inhotim</a></li>
+               <li><a href="/colecoes_en.php" title="previous collections">previous collections</a></li>
+               <!--
              <li><a href="/football_en.php" title="football series">Football Series</a></li>
+             -->
  	          <li><a href="/lojas_en.php" title="Stores">Stores</a></li>
               <li><a href="/oskarmetsavaht_en.php" title="Oskar Metsavaht">Oskar Metsavaht</a></li>
               <li><a href="http://goo.gl/vWFQiq" target="_blank" title="OM.art">OM.art</a></li>
@@ -37,20 +40,26 @@
 		  <?php } elseif ($lang == "jp") { ?>
           	  <li><a href="/inhotim_jp.php" title="Inhotim">Inhotim</a></li>
               <li><a href="http://www.hpfrance.com/Blog/osklen/" target="_blank" title="Blog">ブログ</a></li>
- 	          <!-- FAZER --><li><a href="/football_jp.php" title="football series">Football Series</a></li>
- 	      	  <!-- FAZER --><li><a href="/5521rio_jp.php" title="5521 Rio">+5521 RIO</a></li>
- 	          <!-- FAZER --><li><a href="/intothemountains_jp.php" title="Into the mountains">Into The Mountains</a></li>
-              <!-- FAZER --><li><a href="/lojas_jp.php" title="Stores">店舗</a></li>
-              <!-- FAZER --><li><a href="/oskarmetsavaht_jp.php" title="Oskar Metsavaht">オスカル・メツァヴァト</a></li>
-                          <!--<li><a href="https://ja-jp.facebook.com/osklen.japan" target="_blank" title="facebook">facebook</a></li> -->
+               <li><a href="/colecoes_jp.php" title="previous collections">previous collections</a></li>
+               <!--
+ 	          <li><a href="/football_jp.php" title="football series">Football Series</a></li>
+ 	      	  <li><a href="/5521rio_jp.php" title="5521 Rio">+5521 RIO</a></li>
+ 	          <li><a href="/intothemountains_jp.php" title="Into the mountains">Into The Mountains</a></li>
+              -->
+              <li><a href="/lojas_jp.php" title="Stores">店舗</a></li>
+              <li><a href="/oskarmetsavaht_jp.php" title="Oskar Metsavaht">オスカル・メツァヴァト</a></li>
+              <!--<li><a href="https://ja-jp.facebook.com/osklen.japan" target="_blank" title="facebook">facebook</a></li> -->
              <li><a href="http://www.institutoe.org.br/en/" target="_blank" title="e-fabric">イー・ファブリック</a></li>
               <li><a href="/contato_jp.php" title="Contact">お問い合わせ</a></li>
               <li class="cat-store"><a href="http://www.hpfmall.com/shop/hpfmall/item/list/category_id/3031" target="_blank" title="Shop Online"><span>ウェブ ショップ</span></a></li>              
           <?php } else { ?>
              <li><a href="/fw15.php" title="F/W 15">F/W 15</a></li>
+               <li><a href="/colecoes.php" title="coleções anteriores">coleções anteriores</a></li>
+               <!--
  	         <li><a href="/inhotim.php" title="Inhotim">Inhotim</a></li>
  	         <li><a href="/football.php" title="football series">Football Series</a></li>
- 	         <!-- <li><a href="/5521rio" title="5521 Rio">5521 Rio</a></li> -->
+ 	         <li><a href="/5521rio" title="5521 Rio">5521 Rio</a></li> 
+             	-->
 		  	  <li><a href="/lojas" title="Lojas">Lojas</a></li>
               <li><a href="oskarmetsavaht.php" title="Oskar Metsavaht">Oskar Metsavaht</a></li>
               <li><a href="http://goo.gl/7lxyKc" target="_blank" title="Neo Community">Neo Community</a></li>
